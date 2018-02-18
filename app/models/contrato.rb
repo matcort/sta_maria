65f187f0ec2a2d342baa48ab7ec7fa5740a1ec8e
mat cort <mat.cort@gmail.com>
2018-02-18 21:49:09 +0000

    Callback estado local commit contrato

--- app/models/contrato.rb
+++ app/models/contrato.rb
@@ -1,7 +1,7 @@
 class EstadoValidator < ActiveModel::Validator
   def validate(record)
     if record.estado == true 
-      if Contrato.where(estado:true, local_id: record.local_id).count >= 1
+      if Contrato.where(estado:true, local_id: record.local_id).count >= 1 
         record.errors[:base] << "Ya existe un contrato vigente en este local" 
       end
       
@@ -18,7 +18,11 @@ class Contrato < ApplicationRecord
   
     validates_presence_of :fechaFin, :documento, :arrendatario_id, :administrador_id, :local_id, :montoArriendo, :fechaInicio
     validates_inclusion_of :estado, :in => [true, false]
-    validates_with EstadoValidator, fields: [:estado, :local_id]
+    validates_with EstadoValidator, fields: [:estado, :local_id], on: [:create]
+    
+    after_commit do
+      Local.find(self.local.id).update(estadoArriendo: (self.estado)? true : false)
+    end
    
 
 end
